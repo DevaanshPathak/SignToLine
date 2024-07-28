456a66f238a76728cac16cd1bbd3f654a74bd965
Devaansh Pathak <devaanshpathak08@gmail.com>
2024-07-28 08:20:52 +0530

    Init

--- cnn_model.py
+++ cnn_model.py
@@ -1,10 +1,9 @@
 # Part 1 - Building the CNN
-import tensorflow.keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras import optimizers
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import SGD
 
-# Initialing the CNN
+# Initializing the CNN
 classifier = Sequential()
 
 # Step 1 - Convolution Layer
@@ -13,11 +12,11 @@ classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-# Adding second convolution layer
+# Adding a second convolution layer
 classifier.add(Conv2D(32, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-# Adding 3rd Convolution Layer
+# Adding a third Convolution Layer
 classifier.add(Conv2D(64, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -29,22 +28,19 @@ classifier.add(Dense(256, activation='relu'))
 classifier.add(Dropout(0.5))
 classifier.add(Dense(26, activation='softmax'))
 
-# Compiling The CNN
-classifier.compile(
-    optimizer=optimizers.SGD(lr=0.01),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
+# Compiling the CNN
+classifier.compile(optimizer=SGD(lr=0.01),
+                   loss='categorical_crossentropy',
+                   metrics=['accuracy'])
 
-# Part 2 Fitting the CNN to the image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# Part 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True
-)
+    horizontal_flip=True)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -52,40 +48,39 @@ training_set = train_datagen.flow_from_directory(
     'mydata/training_set',
     target_size=(64, 64),
     batch_size=32,
-    class_mode='categorical'
-)
+    class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory(
     'mydata/test_set',
     target_size=(64, 64),
     batch_size=32,
-    class_mode='categorical'
-)
+    class_mode='categorical')
 
-model = classifier.fit(
+# Fit the classifier
+history = classifier.fit_generator(
     training_set,
-    steps_per_epoch=800,
-    epochs=25,
+    steps_per_epoch=len(training_set),
+    epochs=100,
     validation_data=test_set,
-    validation_steps=6500
-)
+    validation_steps=len(test_set))
 
+# Plotting the training history
 import matplotlib.pyplot as plt
 
-# Summarize history for accuracy
-plt.plot(model.history['accuracy'])
-plt.plot(model.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
-# Summarize history for loss
-plt.plot(model.history['loss'])
-plt.plot(model.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
